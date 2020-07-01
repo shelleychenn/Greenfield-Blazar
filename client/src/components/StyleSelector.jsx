d6b4e4a52a278f490a8a0ce6714dec2891cf3a37
Michael Doudy <michael.doudy@gmail.com>
2020-07-01 11:14:15 -0400

    Add onClick event listener on thumbnails that sets state to current selected style

--- client/src/components/StyleSelector.jsx
+++ client/src/components/StyleSelector.jsx
@@ -7,18 +7,17 @@ const StyleSelector = () => {
 
   useEffect(() => {
     setProductStyles(products.productStyles.results);
-    setSelectedStyle(productStyles && productStyles[0]); //! might need to use the 'default?' key
-  });
+    setSelectedStyle(!!productStyles && productStyles[0]); //! might need to use the 'default?' key
+  }, [productStyles]);
 
   return (
     <div className='container-styleSelector'>
-      <p>STYLE > {selectedStyle && selectedStyle.name.toUpperCase()}</p>
+      <p>STYLE > {!!selectedStyle && selectedStyle.name.toUpperCase()}</p>
       <div>
-        {productStyles && productStyles.map((style) => {
-          return (
-            <img src = {style.photos[0].thumbnail_url}/>
-          )
-        })}
+        {!!productStyles &&
+          productStyles.map((style) => {
+            return <img src={style.photos[0].thumbnail_url} onClick={() => setSelectedStyle(style)} />;
+          })}
       </div>
     </div>
   );
