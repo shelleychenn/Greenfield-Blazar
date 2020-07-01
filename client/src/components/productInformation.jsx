109ecc4cc40644bb10a793b30f9a8fdfea4608fa
Michael Doudy <michael.doudy@gmail.com>
2020-06-30 23:28:05 -0400

    Add state variables, add notes

--- client/src/components/productInformation.jsx
+++ client/src/components/productInformation.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
-import { productInformation as prodInfo, productStyles as prodStyles } from '../../../_testApiData/_productsApi.js';
+import React, { useState, useEffect } from 'react';
+import products from '../../../_testApiData/_productsApi.js';
+//! import reviews test data
 
 const ProductInformation = () => {
-  const [ productInformation, setProductInformation ] = useState({});
-  const [ productStyles, setProductStyles ] = useState({});
+  const [productDetails, setProductDetails] = useState();
+  const [productStyles, setProductStyles] = useState();
+  //! set review state
+
+  useEffect(() => {
+    setProductDetails(products.productInformation);
+    setProductStyles(products.productStyles);
+  });
 
   return (
-    <div className = 'container-productInformation'>
+    <div className='container-productInformation'>
+      //! If no reviews available, hides this section
       <div>
         <div>STARS</div>
-        <p>Read all reviews</p>
+        //! Update # with review count
+        //! Link scrolls the page to the Ratings & Reviews
+        <a href='.'>Read all # reviews</a>
       </div>
-      <p>CATEGORY</p>
-      <h1>Product Name</h1>
-      <p>Price</p>
+      <p>{productDetails ? productDetails.category.toUpperCase() : null}</p>
+      <h1>{productDetails ? productDetails.name : null}</h1>
+      //! Connect price to the currently selected style
+      //! If SKU is discounted, sale price appears in red; strikethrough original price
+      <p>{productDetails ?
+      Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumSignificantDigits: 5 })
+          .format(productDetails.default_price) : null}</p>
     </div>
   );
-}
+};
 
 export default ProductInformation;
