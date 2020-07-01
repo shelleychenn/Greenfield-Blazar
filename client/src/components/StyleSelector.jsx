acc838837aa8595ed24f69848f575dd78fd9953f
Michael Doudy <michael.doudy@gmail.com>
2020-07-01 00:34:16 -0400

    Add basic structure to styleSelector component

---
+++ client/src/components/StyleSelector.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import products from '../../../_testApiData/_productsApi.js';
+
+const StyleSelector = () => {
+  const [productStyles, setProductStyles] = useState();
+  const [selectedStyle, setSelectedStyle] = useState();
+
+  useEffect(() => {
+    setProductStyles(products.productStyles.results);
+    setSelectedStyle(productStyles && productStyles[0]); //! might need to use the 'default?' key
+  });
+
+  return (
+    <div className='container-styleSelector'>
+      <p>STYLE > {selectedStyle && selectedStyle.name.toUpperCase()}</p>
+      <div>
+        {productStyles && productStyles.map((style) => {
+          return (
+            <img src = {style.photos[0].thumbnail_url}/>
+          )
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default StyleSelector;
