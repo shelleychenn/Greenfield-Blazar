503b73562abb50bc459712866158bb5c194ad90a
shelleychenn <shelleychen322@gmail.com>
2020-07-02 23:51:45 -0400

    Display date of review in correct format

--- client/src/components/ReviewListTileFooter.jsx
+++ client/src/components/ReviewListTileFooter.jsx
@@ -3,6 +3,13 @@ import React, { Component } from 'react';
 class ReviewListTileFooter extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      wasHelpfulCount: this.props.review.helpfulness,
+    };
+  }
+
+  incrementWasHelpfulCount() {
+    this.setState({});
   }
 
   render() {
