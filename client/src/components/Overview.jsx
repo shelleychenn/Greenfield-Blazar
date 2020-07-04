baab192eec9c34b758a277b2c08b88256e22a4fd
Michael Doudy <michael.doudy@gmail.com>
2020-07-04 16:34:18 -0400

    Add product information component to overview

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ImageGallery from './overview-components/ImageGallery.jsx';
 import AddToCart from './overview-components/AddToCart.jsx';
+import ProductInformation from './overview-components/ProductInformation.jsx';
 
 const Overview = () => {
   return (
     <>
       <ImageGallery />
       <AddToCart />
+      <ProductInformation />
     </>
   );
 }
