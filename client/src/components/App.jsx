8eb746622f49f792bb6aa01493e5d88f7a9ccb25
Eweiner11 <Eweiner11@gmail.com>
2020-07-09 01:08:49 -0400

    trying to integrate

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -62,8 +62,8 @@ const App = () => {
       <>
         <Header />
         <Overview />
-        {/* <RelatedItemsAndComparison /> */}
-        <QuestionsAndAnswers />
+        <RelatedItemsAndComparison />
+        {/* <QuestionsAndAnswers /> */}
         <RatingsAndReviews />
         <Footer />
       </>
